4dc8db6c8237269b1fbe030189ed5fcc8d33b60b
ABBUGARI POOJITHA <poojithaabbugari041@gmail.com>
2025-10-07 07:20:54 -0300

    Add files via upload

---
+++ Assign.py
@@ -0,0 +1,74 @@
+#Assignment operators
+#Assignment operators are used to assign values to the  variables
+
+#(i) 
+#s = 20
+#print(s) the output is 20
+
+#(ii)
+#h = 3
+#h += 6
+#print(h)
+
+#(iii)
+#r = 10
+#r -= 2
+#print(r)
+
+#(iv)
+#u = 13
+#u *= 2
+#print(u)
+
+#(v)
+#t = 24
+#t /= 4
+#print(t)
+
+#(vi)
+#h = 3
+#h %= 42
+#print(h)
+
+#(vii)
+#i = 44
+#i //= 5
+#print(i)
+
+#(viii)
+#p = 5
+#p **= 3
+#print(p)
+
+#(iX)
+#o = 5
+#o &= 3
+#print(o)
+
+#(X)
+#o = 2
+#o &= 3
+#print(o)
+
+#(xi)
+#j = 5
+#j |= 3
+#print(j)
+
+#(xii)
+#i = 5
+#i ^= 3
+#print(i)
+
+#(xiii)
+#t = 5
+#t >>= 3
+#print(t)
+
+#(xiv)
+#h = 6
+#h <<= 3
+#print(h)
+
+#(xv)
+#print(a := 4)
